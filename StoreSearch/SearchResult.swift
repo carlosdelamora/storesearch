9d6c3b2cf772a67302dccfd99668613bcbba5b16
Carlos De la Mora <carlosdelamorac@gmail.com>
2016-12-20 14:41:18 -0700

    Created a TableView and a SearchBar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResults.swift
+//  StoreSearch
+//
+//  Created by Carlos De la mora on 12/20/16.
+//  Copyright © 2016 carlosdelamora. All rights reserved.
+//
+
+import Foundation
+
+class SearchResults{
+    var name = ""
+    var artistName = ""
+}
