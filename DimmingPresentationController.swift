37b29335e939900fe3b86fbe73aa1bb75ae0e87b
Carlos De la Mora <carlosdelamorac@gmail.com>
2016-12-27 12:47:13 -0700

    Animate the detail view

--- DimmingPresentationController.swift
+++ DimmingPresentationController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+
 // we use this class and set the should Remorve presentation to false
 class DimmingPresentationController: UIPresentationController{
+    
+    
+    lazy var dimmingView = GradientView(frame: CGRect.zero)
+
+    
     override var shouldRemovePresentersView: Bool {
         return false
     }
     
-    lazy var dimingView = GradientView(frame: CGRect.zero)
-    
+    //the animation of the presentation of the gradient
     override func presentationTransitionWillBegin() {
-        dimingView.frame = containerView!.bounds
-        containerView!.insertSubview(dimingView, at: 0)
+        dimmingView.frame = containerView!.bounds
+        containerView!.insertSubview(dimmingView, at: 0)
+        
+        dimmingView.alpha = 0
+        if let coordinator = presentedViewController.transitionCoordinator{
+            coordinator.animate(alongsideTransition:{ _ in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    //the animation of the dismissal of the gradient
+    override func dismissalTransitionWillBegin() {
+        if let coordinator = presentedViewController.transitionCoordinator{
+            coordinator.animate(alongsideTransition:{ _ in
+            self.dimmingView.alpha = 1}, completion: nil)
+        }
     }
 }
